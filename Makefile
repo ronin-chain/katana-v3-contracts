2569cd6efbd643ad86e4e049ebcda2daff2023ad
Thai Xuan Dang <dang.thai@skymavis.com>
2024-11-20 14:00:39 +0700

    feat: transfer ownership of the beacon in mainnet deploy script

--- Makefile
+++ Makefile
@@ -5,3 +5,11 @@ deploy-testnet:
 deploy-testnet-broadcast:
 	op run --env-file="./.env" -- \
 	forge script DeployKatanaV3Testnet -f ronin-testnet --verify --verifier sourcify --verifier-url https://sourcify.roninchain.com/server/ --legacy --broadcast
+
+deploy-mainnet:
+	op run --env-file="./.env" -- \
+	forge script DeployKatanaV3Mainnet -f ronin-mainnet
+
+deploy-mainnet-broadcast:
+	op run --env-file="./.env" -- \
+	forge script DeployKatanaV3Mainnet -f ronin-mainnet --verify --verifier sourcify --verifier-url https://sourcify.roninchain.com/server/ --legacy --broadcast
--- script/ronin-mainnet/DeployKatanaV3Mainnet.s.sol
+++ script/ronin-mainnet/DeployKatanaV3Mainnet.s.sol
@@ -5,11 +5,14 @@ import { Script } from "forge-std/Script.sol";
 import { console } from "forge-std/console.sol";
 import { DeployKatanaV3Periphery } from "../DeployKatanaV3Periphery.s.sol";
 import { MixedRouteQuoterV1 } from "src/periphery/lens/MixedRouteQuoterV1.sol";
+import { KatanaV3PoolBeacon } from "src/core/KatanaV3PoolBeacon.sol";
 
 contract DeployKatanaV3Mainnet is DeployKatanaV3Periphery {
   address mixedRouteQuoterV1;
+  address multisig;
 
   function setUp() public override {
+    multisig = 0x9D05D1F5b0424F8fDE534BC196FFB6Dd211D902a; // Multisig
     proxyAdmin = 0xA3e7d085E65CB0B916f6717da876b7bE5cC92f03; // Proxy Admin
     governance = 0x2C1726346d83cBF848bD3C2B208ec70d32a9E44a; // Governance Proxy
     treasury = 0x22cEfc91E9b7c0f3890eBf9527EA89053490694e; // Ronin Treasury
@@ -24,8 +27,12 @@ contract DeployKatanaV3Mainnet is DeployKatanaV3Periphery {
   function run() public override {
     super.run();
 
-    vm.broadcast();
+    vm.startBroadcast();
     mixedRouteQuoterV1 = address(new MixedRouteQuoterV1(factory, factoryV2, wron));
     console.log("MixedRouteQuoterV1 deployed:", mixedRouteQuoterV1);
+
+    KatanaV3PoolBeacon(beacon).transferOwnership(multisig);
+    console.log("Transfered ownership of KatanaV3PoolBeacon to", multisig);
+    vm.stopBroadcast();
   }
 }
