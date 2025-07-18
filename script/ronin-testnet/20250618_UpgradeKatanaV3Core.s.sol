fe2985c02c397c091e978a24409471ca92236241
huyhuynh3103 <huy.huynh@skymavis.com>
2025-07-18 15:20:26 +0700

    feat(V3Migrator): add check pair since the Ve Migrator is immutable

--- script/ronin-testnet/20250618_UpgradeKatanaV3Core.s.sol
+++ script/ronin-testnet/20250618_UpgradeKatanaV3Core.s.sol
@@ -2,16 +2,30 @@
 pragma solidity ^0.7.6;
 
 import { Script } from "forge-std/Script.sol";
+import { TransparentUpgradeableProxy } from "@openzeppelin/contracts/proxy/TransparentUpgradeableProxy.sol";
 import { KatanaV3Pool } from "src/core/KatanaV3Pool.sol";
+import { KatanaV3Factory } from "src/core/KatanaV3Factory.sol";
 import { KatanaV3PoolBeacon } from "src/core/KatanaV3PoolBeacon.sol";
+import { NonfungiblePositionManager } from "src/periphery/NonfungiblePositionManager.sol";
 
 contract Migration__20250618_UpgradeKatanaV3Core is Script {
+  address positionManager = 0x7C2716803c09cd5eeD78Ba40117084af3c803565;
+  address factory;
+  address beacon;
+
+  function setUp() public {
+    NonfungiblePositionManager positionManagerContract = NonfungiblePositionManager(positionManager);
+    factory = positionManagerContract.factory();
+    KatanaV3Factory katanaV3Factory = KatanaV3Factory(factory);
+    beacon = katanaV3Factory.beacon();
+  }
+
   function run() public {
     address implementation = address(new KatanaV3Pool());
-    KatanaV3PoolBeacon beacon = KatanaV3PoolBeacon(0x3cE42fE62db79401aC5E6466Fc9B769e4f68e419);
 
     vm.startBroadcast();
-    beacon.upgradeTo(implementation);
+    TransparentUpgradeableProxy(factory).upgradeTo(address(new KatanaV3Factory()));
+    KatanaV3PoolBeacon(beacon).upgradeTo(implementation);
     vm.stopBroadcast();
   }
 }
